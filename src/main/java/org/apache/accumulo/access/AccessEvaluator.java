c3c223b5c0fc96e839e91daeb18efa9613925ea3
DomGarguilo <dominic.garguilo@gmail.com>
2023-10-13 14:01:24 -0400

    Format and rearrange additional javadoc point

--- src/main/java/org/apache/accumulo/access/AccessEvaluator.java
+++ src/main/java/org/apache/accumulo/access/AccessEvaluator.java
@@ -28,7 +28,10 @@ import java.util.Collection;
  * <p>
  * Note: For performance improvements, especially in cases where expressions are expected to repeat,
  * it's recommended to wrap this evaluator with an external caching mechanism, such as Guava's
- * cache, to leverage its extensive caching options.  Caching is only safe under the assumption that for an AccessEvaluator instance that evaluating the same expression multiple times will always yield the same result. When considering caching, may need to mitigate any environmental factors that could change this assumption.
+ * cache, to leverage its extensive caching options. Caching is only safe under the assumption that
+ * for an AccessEvaluator instance, evaluating the same expression multiple times will always yield
+ * the same result. When considering caching, any environmental factors that might change this
+ * assumption may need to be mitigated.
  * </p>
  *
  * Below is an example that should print false and then print true.
