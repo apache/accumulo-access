f7b0aa9a8f8a97844f592667e029f73abedf8ae0
Keith Turner <kturner@apache.org>
2026-01-09 23:31:44 +0000

    remove todo

--- core/src/main/java/org/apache/accumulo/access/AccessExpression.java
+++ core/src/main/java/org/apache/accumulo/access/AccessExpression.java
@@ -22,74 +22,9 @@ import java.io.Serializable;
 
 import org.apache.accumulo.access.impl.AccessExpressionImpl;
 
-// TODO update or remove example code... maybe remove it because the project has example code that is tested
 /**
- * This class offers the ability to operate on access expressions.
+ * An immutable wrapper for a validated access expression.
  *
- * <p>
- * Below is an example of how to use this API.
- *
- * <pre>
- * {@code
- * // The following authorization does not need quoting
- * // so the return value is the same as the input.
- * var auth1 = AccessExpression.quote("CAT");
- *
- * // The following two authorizations need quoting and the return values will be quoted.
- * var auth2 = AccessExpression.quote("🦕");
- * var auth3 = AccessExpression.quote("🦖");
- *
- * // Create an AccessExpression using auth1, auth2, and auth3
- * var exp = "(" + auth1 + "&" + auth3 + ")|(" + auth1 + "&" + auth2 + ")";
- *
- * // Validate the expression w/o creating an object
- * AccessExpression.validate(exp);
- * System.out.println(exp);
- *
- * // Validate the expression and create an immutable AccessExpression object.  This object can be passed around in code and other code knows it valid and does not need to revalidate.
- * AccessExpression accessExpression = AccessExpression.of(exp);
- * System.out.println(accessExpression);
- *
- * // Print the authorization in the expression
- * AccessExpression.findAuthorizations(exp, System.out::println);
- *
- * // Create an AccessExpression with a parse tree.  Creating this is more expensive than calling AccessExpression.of(), so it should only be used if the parse tree is needed.
- * ParsedAccessExpression parsed = AccessExpression.parse(exp);
- * System.out.println("type:"+parsed.getType()+" child[0]:"+parsed.getChildren().get(0)+" child[1]:"+  child[1]:"+parsed.getChildren().get(1));
- *
- * }
- * </pre>
- *
- * The above example will print the following.
- *
- * <pre>
- * {@code
- * (CAT&"🦖")|(CAT&"🦕")
- * (CAT&"🦖")|(CAT&"🦕")
- * CAT
- * 🦖
- * CAT
- * 🦕
- * type:OR child[0]:CAT&"🦖" child[1]:CAT&"🦕"
- * }
- * </pre>
- *
- * The following code will throw an {@link InvalidAccessExpressionException} because the expression
- * is not valid.
- *
- * <pre>
- * {@code
- * AccessExpression.validate("A&B|C");
- * }
- * </pre>
- *
- * <p>
- * Instances of this class are thread-safe.
- *
- * <p>
- * Note: The underlying implementation uses UTF-8 when converting between bytes and Strings.
- *
- * @see <a href="https://github.com/apache/accumulo-access">Accumulo Access Documentation</a>
  * @since 1.0.0
  */
 public sealed abstract class AccessExpression implements Serializable
