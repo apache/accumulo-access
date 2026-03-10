8d6f6e1e30d90a7a9b567c84c7100eb7e18c9e4c
Dave Marion <dlmarion@apache.org>
2026-03-10 19:18:48 +0000

    Implement PR suggestion

--- modules/core/src/main/java/org/apache/accumulo/access/impl/AccessExpressionImpl.java
+++ modules/core/src/main/java/org/apache/accumulo/access/impl/AccessExpressionImpl.java
@@ -79,10 +79,7 @@ public final class AccessExpressionImpl extends AccessExpression {
   public static CharSequence unquote(CharSequence term) {
     int len = term.length();
     if (len >= 2 && term.charAt(0) == '"' && term.charAt(len - 1) == '"') {
-      if (len == 2) {
-        throw new IllegalArgumentException("Empty strings are not legal authorizations.");
-      }
-      term = AccessEvaluatorImpl.unescape(term.subSequence(1, term.length() - 1));
+      term = len == 2 ? "" : AccessEvaluatorImpl.unescape(term.subSequence(1, term.length() - 1));
     }
     if (term.isEmpty()) {
       throw new IllegalArgumentException("Empty strings are not legal authorizations.");
